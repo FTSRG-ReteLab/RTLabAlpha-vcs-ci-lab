3d71262939109062ccf02ad0fb51102a1a953b1a
meres <sealdolphin@sch.bme.hu>
2019-02-11 08:44:40 -0800

    Added TachoTest
    Added new unit test to test TachoTable implementation

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.train.system;
 
+import hu.bme.mit.train.controller.Tachograph;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,13 +15,15 @@ public class TrainSystemTest {
 	TrainController controller;
 	TrainSensor sensor;
 	TrainUser user;
-	
+	Tachograph table;
+
 	@Before
 	public void before() {
 		TrainSystem system = new TrainSystem();
 		controller = system.getController();
 		sensor = system.getSensor();
 		user = system.getUser();
+		table = new Tachograph(controller,user);
 
 		sensor.overrideSpeedLimit(50);
 	}
@@ -60,4 +63,17 @@ public class TrainSystemTest {
 		controller.followSpeed();
 		Assert.assertEquals(8, controller.getReferenceSpeed());
 	}
+
+	@Test
+	public void TachoTest(){
+
+		user.overrideJoystickPosition(5);
+		controller.followSpeed();
+		table.step();
+		Assert.assertEquals(5,table.getSpeed(0));
+		Assert.assertEquals(5,table.getPosition(0));
+
+
+	}
+
 }
